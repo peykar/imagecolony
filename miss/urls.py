e8f7990bad9ded5c44e9ff69137f1040e5819cca
Peyman Karimi <peykar@gmail.com>
2013-04-22 01:27:19 +0430

    urls fixed.

--- miss/urls.py
+++ miss/urls.py
@@ -9,12 +9,12 @@ urlpatterns = patterns('',
     url(r'^register$', views.register_view, name='register'),
     url(r'^user_info$', views.user_info_view, name='user_info'),
 
-    url(r'^applications$', views.types_view, name='types', {'_type': 'application'}),
-    url(r'^imageries$', views.types_view, name='types', {'_type': 'imagery'}),
-    url(r'^imagery_problems$', views.types_view, name='types', {'_type': 'imagery_problem'}),
+    url(r'^applications$', views.types_view, {'_type': 'application'}, name='applications'),
+    url(r'^imageries$', views.types_view, {'_type': 'imagery'}, name='imageries'),
+    url(r'^imagery_problems$', views.types_view, {'_type': 'imagery_problem'}, name='imagery_problems'),
 
     url(r'^region/add$', views.add_region_view, name='add_region'),
-    url(r'^vote/up/(?P<region_id>\d+)$', views.add_vote_view, name='add_vote', {'vote': 'up'}),
-    url(r'^vote/down/(?P<region_id>\d+)$', views.add_vote_view, name='add_vote', {'vote': 'down'}),
-    url(r'^aim/support/(?P<region_id>\d+)$', views.support_aim_view, name='support_aim',),
+    url(r'^vote/up/(?P<region_id>\d+)$', views.add_vote_view, {'vote': 'up'}, name='add_up_vote'),
+    url(r'^vote/down/(?P<region_id>\d+)$', views.add_vote_view, {'vote': 'down'}, name='add_down_vote'),
+    url(r'^aim/support/(?P<region_id>\d+)$', views.support_aim_view, name='support_aim'),
 )
