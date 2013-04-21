e2bb5fd9ede9e6f75fcf1e5e64d786938204321a
Peyman Karimi <peykar@gmail.com>
2013-04-21 16:13:54 +0430

    layers changed.

--- miss/static/miss/js/app.js
+++ miss/static/miss/js/app.js
@@ -8,21 +8,8 @@ function init() {
     });
 
     var osm = new OpenLayers.Layer.OSM();
-/*    var google_roadmap = new OpenLayers.Layer.Google("Google Roadmap", {type: google.maps.MapTypeId.ROADMAP});
-    var google_hybrid = new OpenLayers.Layer.Google("Google Hybrid", {type: google.maps.MapTypeId.HYBRID});
-    var google_terrain = new OpenLayers.Layer.Google("Google Terrain", {type: google.maps.MapTypeId.TERRAIN});
+    var google_roadmap = new OpenLayers.Layer.Google("Google Roadmap", {type: google.maps.MapTypeId.ROADMAP});
     var google_satellite = new OpenLayers.Layer.Google("Google Satellite", {type: google.maps.MapTypeId.SATELLITE});
-
-    var bing_road = new OpenLayers.Layer.Bing({
-        name: "Bing Road",
-        key: bing_apiKey,
-        type: "Road"
-    });
-    var bing_hybrid = new OpenLayers.Layer.Bing({
-        name: "Bing Hybrid",
-        key: bing_apiKey,
-        type: "AerialWithLabels"
-    });
     var bing_aerial = new OpenLayers.Layer.Bing({
         name: "Bing Aerial",
         key: bing_apiKey,
@@ -30,10 +17,8 @@ function init() {
     });
 
     // note that first layer must be visible
-    map.addLayers([osm, google_roadmap, google_hybrid, google_terrain, google_satellite, bing_road, bing_hybrid, bing_aerial]);
-*/
+    map.addLayers([osm, google_satellite, bing_aerial]);
 
-    map.addLayers([osm]);
     map.addControl(new OpenLayers.Control.LayerSwitcher());
     map.zoomToMaxExtent();
 
