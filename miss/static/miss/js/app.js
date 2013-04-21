32e3c10d62316a8a7038e4ec85263847444e00bb
Fardin Koochaki <me@fardinak.com>
2013-04-21 20:30:03 +0430

    Point selector

--- miss/static/miss/js/app.js
+++ miss/static/miss/js/app.js
@@ -1,28 +1,40 @@
 var bing_apiKey = "AqTGBsziZHIJYYxgivLBf0hVdrAk9mWO5cQcb8Yux8sW5M8c8opEC2lZqKR1ZZXf";
-var map;
+var map, layers, controls;
 
 function init() {
-    map = new OpenLayers.Map({
-        div: "map",
-    });
+  map = new OpenLayers.Map({
+      div: "map",
+  });
 
-    var google_roadmap = new OpenLayers.Layer.Google("Google Roadmap", {type: google.maps.MapTypeId.ROADMAP});
-    var google_satellite = new OpenLayers.Layer.Google("Google Satellite", {type: google.maps.MapTypeId.SATELLITE});
-    var bing_aerial = new OpenLayers.Layer.Bing({
+  // Layers
+  layers = {
+    //google_roadmap: new OpenLayers.Layer.Google("Google Roadmap", {type: google.maps.MapTypeId.ROADMAP}),
+    google_satellite: new OpenLayers.Layer.Google("Google Satellite", {type: google.maps.MapTypeId.SATELLITE}),
+    bing_aerial: new OpenLayers.Layer.Bing({
         name: "Bing Aerial",
         key: bing_apiKey,
         type: "Aerial"
-    });
+    })
+  }
+  var layersArray = toArray(layers);
 
-    // note that first layer must be visible
-    map.addLayers([google_satellite, bing_aerial]);
+  // Controls
+  controls = {
+    layerSwitcher: new OpenLayers.Control.LayerSwitcher(),
+    point: new OpenLayers.Control.DrawFeature(layersArray, OpenLayers.Handler.Point)
+  }
+  var controlsArray = toArray(controls);
 
-    map.addControl(new OpenLayers.Control.LayerSwitcher());
-    map.zoomToMaxExtent();
+  // note that first layer must be visible
+  map.addLayers(layersArray);
+  map.addControls(controlsArray);
 
+  map.zoomToMaxExtent();
+}
 
-    //map = new OpenLayers.Map("map");
-    
+// cast object to array
+function toArray(obj) {
+  return $.map(obj, function(x) { return x; });
 }
 
 // Show messages
@@ -31,6 +43,7 @@ function show(type, m) {
   $(dom).appendTo('body');
   dom = $(".alert").alert();
 
+  // Remove the message after 8 seconds
   setTimeout(function() {
     $(this).alert('close');
   }.bind(dom), 8000);
@@ -102,7 +115,7 @@ var authentication = {
 };
 
 
-// Do
+// Authentication
 $(function() {
   authentication.check();
   $("#logout").click(function(e) {
@@ -236,5 +249,13 @@ $(function() {
         form.find(".loading").remove();
       }
     });
+  });
+});
+
+
+// Do
+$(function() {
+  $('a[href="#add"][data-toggle="tab"]').on('show', function(e) {
+    controls.point.activate();
   });
 });
