64f62c652c71ed8afab28b0db92d889ef2e30491
Peyman Karimi <peykar@gmail.com>
2013-04-21 15:39:41 +0430

    conflit problem.

--- mission_earth/wsgi.py
+++ mission_earth/wsgi.py
@@ -24,7 +24,8 @@ sys.path.append(project)
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "mission_earth.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mission_earth.settings")
+# Do not conflict with other django sites.
+os.environ["DJANGO_SETTINGS_MODULE"] = "mission_earth.settings"
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
