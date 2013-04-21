17c169d4891894803e7c65566cf251ae3625eb67
Peyman Karimi <peykar@gmail.com>
2013-04-21 21:41:46 +0430

    application type.

--- miss/models.py
+++ miss/models.py
@@ -41,7 +41,7 @@ class Region(models.Model):
     region = models.PointField(db_column='the_geom')
     when_to_take = models.CharField('when to take imagery', max_length=255)
 
-    priority = models.CharField(max_length=3, choices=PRIORITY_TYPES)
+    application = models.CharField(max_length=3, choices=APPLICATION_TYPES)
     imagery_type = models.CharField(max_length=3, choices=IMAGERY_TYPES)
     current_imagery_problem = models.CharField(max_length=3, choices=IMAGERY_PROBLEM_TYPES)
     objects = models.GeoManager()
