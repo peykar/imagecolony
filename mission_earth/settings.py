cab1a0934e46292363ce105e81d94d8a2aead28c
Vahid Negahdari <vahid.negahdari@gmail.com>
2013-04-22 04:58:56 +0430

    reset settings to default

--- mission_earth/settings.py
+++ mission_earth/settings.py
@@ -15,8 +15,8 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'mission_earth',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'mission_earth',
-        'PASSWORD': 'me',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
